ce1cc1282a3e146f9470e257ea913e278005d07a
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2023-11-28 16:50:12 -0500

    React reset setup

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import ExerciseDetail from './pages/ExerciseDetail';
-import Home from './pages/Home';
 import Navbar from './components/Navbar'
+import Home from './pages/home'
+import Footer from './components/Footer'
 
 
 
 const App = () => {
   return (
-    <Box width ="400px">
-      Navbar
+    <div>
+    <Box width="400px" sx = {{ width: { xl: '1488px' }}} m = "auto"> 
+      <Navbar />
       <Routes>
-        <Route path = "/" element={<Home/>} />
-        <Route path = "/exercise/:id" element= {<ExerciseDetail />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
-  )
+  </div>
+);
 }
 
 export default App
